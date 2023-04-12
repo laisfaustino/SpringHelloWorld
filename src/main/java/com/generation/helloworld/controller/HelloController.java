96855b1a42e61a19cabd41f3c969e9f384ff9f26
Lais Faustino <laisfaustino@hotmail.com>
2023-04-12 13:18:26 -0300

    Add files via upload

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -15,6 +15,15 @@ public class HelloController {
 	@GetMapping("/nome")
 	public String meuNome() {
 		return "<b>Lais!</b>";
-	}		
+	}
+	@GetMapping("/bsm")
+	public String bsm() {
+		return "<br>Mentalidade de Crescimento</b><br>Persistencia<br>Responsabilidade Pessoal<br>Orientação ao Futuro<br>Comunicação<br>Orientação ao Detalhe<br>Proatividade<br>Trabalho em Equipe";
 }
+	@GetMapping("/objetivo")
+	public String objetivo() {
+		return "<br>Realizar todas as atividades propostas</b><br>Ter mais foco para compreender todo o conteudo<br>Avaliar meu autoconhecimento<br>Ter diciplina para realizar todas tarefas diarias";
+	}
+}
+
 	
